179746df49d54897371ff6f493e77c5705ea3819
nikolett.topolics <nikolett.topolics@gmail.com>
2025-03-22 16:09:33 +0100

    landing-page-ready

---
+++ src/app/recipes.mock.ts
@@ -0,0 +1,64 @@
+import { Recipe } from './app.models';
+
+export const mockRecipes: Recipe[] = [
+  {
+    id: 1,
+    name: 'Pistachio Macaron',
+    ingredients: [
+      'eggs',
+      'almond flour',
+      'sugar',
+      'food coloring',
+      'pistachio cream',
+    ],
+    description:
+      'Integer id orci fermentum, egestas lacus at, hendrerit turpis. Quisque in orci semper, aliquet neque vel, aliquam velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam accumsan eleifend nisl ac pulvinar. Nam posuere eu ipsum quis porttitor. Donec imperdiet libero quis turpis tincidunt, aliquet mattis ante semper. Vestibulum commodo velit at tempor condimentum. Pellentesque dapibus arcu dolor, convallis pellentesque lorem luctus ut. Vestibulum magna urna, luctus quis dolor mollis, varius varius justo. Quisque congue, sapien et ornare fermentum, neque risus volutpat libero, ut maximus mauris urna in leo. Sed nisi dui, blandit et aliquet sit amet, mattis porttitor ipsum. Phasellus feugiat interdum urna et tempus.',
+  },
+  {
+    id: 2,
+    name: 'Chocolate Macaron',
+    ingredients: [
+      'eggs',
+      'almond flour',
+      'sugar',
+      'food coloring',
+      'chocolate',
+    ],
+    description:
+      'Integer id orci fermentum, egestas lacus at, hendrerit turpis. Quisque in orci semper, aliquet neque vel, aliquam velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam accumsan eleifend nisl ac pulvinar. Nam posuere eu ipsum quis porttitor. Donec imperdiet libero quis turpis tincidunt, aliquet mattis ante semper. Vestibulum commodo velit at tempor condimentum. Pellentesque dapibus arcu dolor, convallis pellentesque lorem luctus ut. Vestibulum magna urna, luctus quis dolor mollis, varius varius justo. Quisque congue, sapien et ornare fermentum, neque risus volutpat libero, ut maximus mauris urna in leo. Sed nisi dui, blandit et aliquet sit amet, mattis porttitor ipsum. Phasellus feugiat interdum urna et tempus.',
+  },
+  {
+    id: 3,
+    name: 'Raspberry Macaron',
+    ingredients: [
+      'eggs',
+      'almond flour',
+      'sugar',
+      'food coloring',
+      'raspberry',
+    ],
+    description:
+      'Integer id orci fermentum, egestas lacus at, hendrerit turpis. Quisque in orci semper, aliquet neque vel, aliquam velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam accumsan eleifend nisl ac pulvinar. Nam posuere eu ipsum quis porttitor. Donec imperdiet libero quis turpis tincidunt, aliquet mattis ante semper. Vestibulum commodo velit at tempor condimentum. Pellentesque dapibus arcu dolor, convallis pellentesque lorem luctus ut. Vestibulum magna urna, luctus quis dolor mollis, varius varius justo. Quisque congue, sapien et ornare fermentum, neque risus volutpat libero, ut maximus mauris urna in leo. Sed nisi dui, blandit et aliquet sit amet, mattis porttitor ipsum. Phasellus feugiat interdum urna et tempus.',
+  },
+  {
+    id: 4,
+    name: 'Lemon Macaron',
+    ingredients: ['eggs', 'almond flour', 'sugar', 'food coloring', 'lemon'],
+    description:
+      'Integer id orci fermentum, egestas lacus at, hendrerit turpis. Quisque in orci semper, aliquet neque vel, aliquam velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam accumsan eleifend nisl ac pulvinar. Nam posuere eu ipsum quis porttitor. Donec imperdiet libero quis turpis tincidunt, aliquet mattis ante semper. Vestibulum commodo velit at tempor condimentum. Pellentesque dapibus arcu dolor, convallis pellentesque lorem luctus ut. Vestibulum magna urna, luctus quis dolor mollis, varius varius justo. Quisque congue, sapien et ornare fermentum, neque risus volutpat libero, ut maximus mauris urna in leo. Sed nisi dui, blandit et aliquet sit amet, mattis porttitor ipsum. Phasellus feugiat interdum urna et tempus.',
+  },
+  {
+    id: 5,
+    name: 'Vanilla Macaron',
+    ingredients: ['eggs', 'almond flour', 'sugar', 'food coloring'],
+    description:
+      'Integer id orci fermentum, egestas lacus at, hendrerit turpis. Quisque in orci semper, aliquet neque vel, aliquam velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam accumsan eleifend nisl ac pulvinar. Nam posuere eu ipsum quis porttitor. Donec imperdiet libero quis turpis tincidunt, aliquet mattis ante semper. Vestibulum commodo velit at tempor condimentum. Pellentesque dapibus arcu dolor, convallis pellentesque lorem luctus ut. Vestibulum magna urna, luctus quis dolor mollis, varius varius justo. Quisque congue, sapien et ornare fermentum, neque risus volutpat libero, ut maximus mauris urna in leo. Sed nisi dui, blandit et aliquet sit amet, mattis porttitor ipsum. Phasellus feugiat interdum urna et tempus.',
+  },
+  {
+    id: 6,
+    name: 'Strawberry Macaron',
+    ingredients: ['eggs', 'almond flour', 'sugar', 'food coloring'],
+    description:
+      'Integer id orci fermentum, egestas lacus at, hendrerit turpis. Quisque in orci semper, aliquet neque vel, aliquam velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam accumsan eleifend nisl ac pulvinar. Nam posuere eu ipsum quis porttitor. Donec imperdiet libero quis turpis tincidunt, aliquet mattis ante semper. Vestibulum commodo velit at tempor condimentum. Pellentesque dapibus arcu dolor, convallis pellentesque lorem luctus ut. Vestibulum magna urna, luctus quis dolor mollis, varius varius justo. Quisque congue, sapien et ornare fermentum, neque risus volutpat libero, ut maximus mauris urna in leo. Sed nisi dui, blandit et aliquet sit amet, mattis porttitor ipsum. Phasellus feugiat interdum urna et tempus.',
+  },
+];
