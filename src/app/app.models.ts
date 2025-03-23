fc26cebca4ed47e3d88db36b3ddf7a0d3f8815f3
nikolett.topolics <nikolett.topolics@gmail.com>
2025-03-23 21:31:41 +0100

    recipe page setup in progress

--- src/app/app.models.ts
+++ src/app/app.models.ts
@@ -1,6 +1,8 @@
 export interface Recipe {
-    readonly id: number;
-    readonly name: string;
-    readonly ingredients: string[];
-    readonly description: string;
+  readonly id: number;
+  readonly name: string;
+  readonly ingredients: string[];
+  readonly description: string;
+  readonly imgSrc: string;
+  readonly cardContent: string;
 }
